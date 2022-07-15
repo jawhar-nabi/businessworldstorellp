8dd1fbf344c5965218686129af9a4a5b36a074b4
Ahmedbenkhalifa <ahmedbenkhalifa1997@gmail.com>
2022-07-15 16:08:34 +0100

    translation commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import './App.css';
-
+import './i18n';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React from "react";
 // import sections
-import Hero from '../components/sections/Hero';
-import FeaturesTiles from '../components/sections/FeaturesTiles';
-import FeaturesSplit from '../components/sections/FeaturesSplit';
-import Testimonial from '../components/sections/Testimonial';
-import Cta from '../components/sections/Cta';
+import Hero from "../components/sections/Hero";
+import FeaturesTiles from "../components/sections/FeaturesTiles";
+import FeaturesSplit from "../components/sections/FeaturesSplit";
+import Testimonial from "../components/sections/Testimonial";
+import Cta from "../components/sections/Cta";
+import { useTranslation } from "react-i18next";
 
 const Home = () => {
+  const { t } = useTranslation();
 
   return (
     <>
+      {t('home_welcome')}
       <Hero className="illustration-section-01" />
       <FeaturesTiles id="solutions" />
-      <FeaturesSplit id="experts" invertMobile topDivider imageFill className="illustration-section-02" />
+      <FeaturesSplit
+        id="experts"
+        invertMobile
+        topDivider
+        imageFill
+        className="illustration-section-02"
+      />
       <Testimonial topDivider id="testimonials" />
       <Cta split />
     </>
   );
-}
+};
 
 export default Home;
